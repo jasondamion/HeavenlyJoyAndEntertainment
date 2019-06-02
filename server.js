660e96e9832151fc4fe24f9b3bf6a8979b4ea5b3
Jason Nelson <Jasonnelson11@gmail.com>
2019-06-02 12:58:26 -0400

    Fix

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var PORT = process.env.PORT || 8080;
 var cloudinary = require('cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.cloud_name,
-    api_key: process.env.api_key,
-    api_secret: process.env.api_secret
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret
 }
 );
 
@@ -22,22 +22,18 @@ console.log("Server working")
 
 console.log("Server Connected")
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.send("Working");
 })
 
-app.get("/gallery", function(req, res){
-  cloudinary.v2.api.resources_by_tag('test',
-    {max_results: 20},
-        function (err, results) {
-            res.json(results)
-        }
-)  
+app.get("/gallery", function (req, res) {
+  cloudinary.v2.api.resources_by_tag("test",
+    function (error, result) { console.log(result); });
 })
 
 
 
 
-        
+
 app.listen(PORT);
 
