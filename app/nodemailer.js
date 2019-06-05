caddc8c45542dff06b8dd0351e68eccde5501a40
Jason Nelson <Jasonnelson11@gmail.com>
2019-06-04 20:08:27 -0400

    Added Email

---
+++ app/nodemailer.js
@@ -0,0 +1,64 @@
+var nodemailer = require("nodemailer");
+
+console.log("Connected");
+
+  var mailer = {
+    reserve(gender1, guardianName1, childName1, phone1, address1, email1, childBday1, event1, age1, package1, theme1, packagePrice1, addOnPrice1, totalPrice1, deposit1) {
+
+      var gender = "<br><h1>Gender: " + gender1 + "</h1><br>";
+      var guardianName = "<h1>Guardian Name: " + guardianName1 + "</h1><br>";
+      var childName = "<h1>Child Name: " + childName1 + "</h1><br>";
+      var phone = "<h1>Phone: " + phone1 + "</h1><br>";
+      var address = "<h1>Address: " + address1 + "</h1><br>";
+      var email = "<h1>Email: " + email1 + "</h1><br>";
+      var childBday = "<h1>Child Birthday: " + childBday1 + "</h1><br>";
+      var event = "<h1>Event Date: " + event1 + "</h1><br>";
+      var age = "<h1>Age: " + age1 + "</h1><br>";
+      var package = "<h1>Package Chosen: " + package1 + "</h1><br>";
+      var theme = "<h1>Theme: " + theme1 + "</h1><br>";
+      var packagePrice = "<h1>Package Price: " + "$" + packagePrice1 + "</h1><br>";
+      var addOnPrice = "<h1>Add-On Price: " + "$" + addOnPrice1 + "</h1><br>";
+      var totalPrice = "<h1>Total Price: " + "$" + totalPrice1 + "</h1><br>";
+      var deposit = "<h1>Deposit to be made: " + "$" + deposit1 + "</h1><br>";
+      var fullEmail = gender + guardianName + childName + phone + address + email + childBday + event + age + package + theme + packagePrice + addOnPrice + totalPrice + deposit;
+
+      // async..await is not allowed in global scope, must use a wrapper
+      async function main() {
+        // create reusable transporter object using gmail
+        let transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: "jasonnelson11@gmail.com",
+            pass: 'sharainelson'
+          }
+        });
+
+        // send mail with defined transport object
+        // let info = await transporter.sendMail({
+        //   from: '"Heavenly Joy and Entertainment" <jasonnelson11@gmail.com>', // sender address
+        //   to: "jasonfswd@gmail.com", // list of receivers
+        //   subject: "Reservation", // Subject line
+        //   text: "Reservation: ", // plain text body
+        //   html: fullEmail // html body
+        // });
+        const mailOptions = {
+          from: 'jasonnelson11@gmail.com', // sender address
+          to: 'adrian@heavenlyjoyevents.com', // list of receivers
+          subject: 'Reservation', // Subject line
+          html: fullEmail// plain text body
+        };
+
+        transporter.sendMail(mailOptions, function (err, info) {
+          if(err)
+            console.log(err)
+          else
+            console.log("Email Send: "+ info.response);
+       });
+
+      }
+
+      main().catch(console.error);
+    }
+  }
+
+module.exports =  mailer;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 var cloudinary = require('cloudinary');
 
+// reservation/Male/Muriel Page/Jason Nelson/3477929311/377 New Milford Ave/jasonnelson11@gmail.com/02081999/06032019/21/Silver Package/Outer Space Theme/250/250/500/125
+
 cloudinary.config({
   cloud_name: process.env.cloud_name,
   api_key: process.env.api_key,
@@ -16,6 +18,8 @@ cloudinary.config({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+var mailer = require("./app/nodemailer");
+
 // Routes
 console.log("Server working")
 
@@ -26,7 +30,26 @@ app.get("/", function (req, res) {
   res.send("Working");
 })
 
-
+app.get("/Reservation/:gender/:guardianName/:childName/:phone/:address/:email/:childBday/:event/:age/:package/:theme/:packagePrice/:addOnPrice/:totalPrice/:deposit", function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://jasondamion.github.io');
+  gender = req.params.gender
+  guardianName = req.params.guardianName
+  childName = req.params.childName
+ phone = req.params.phone
+ address = req.params.address
+ email = req.params.email
+ childBday = req.params.childBday
+  event = req.params.event
+  age = req.params.age
+  package1 = req.params.package
+  theme = req.params.theme
+  packagePrice = req.params.packagePrice
+  addOnPrice = req.params.addOnPrice
+  totalPrice = req.params.totalPrice
+  deposit = req.params.deposit
+  mailer.reserve(gender, guardianName, childName, phone, address, email, childBday, event, age, package1, theme, packagePrice, addOnPrice, totalPrice, deposit)
+res.send("Email Sent")
+})
 
 app.get("/gallery", function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'https://jasondamion.github.io');
